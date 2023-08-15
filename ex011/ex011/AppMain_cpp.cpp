5cb739b333056d24abe59bb074ea12d88425fc9a
4S4T <138412409+4S4T@users.noreply.github.com>
2023-08-15 10:40:41 +0900

    8/15

--- ex011/ex011/AppMain_cpp.cpp
+++ ex011/ex011/AppMain_cpp.cpp
@@ -45,7 +45,7 @@
 
 *****************************/
 
-int WINAPI WinMain(_In_HINSTANCE hInstance, _In_opt_HINSTANCE hPrevInstance, _In_LPSTR lp CmdLine, _In_int nShowCmd) {
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
 
 	// ウィンドウタイトル設定
 	SetMainWindowText("Match 3 Puzzle");
